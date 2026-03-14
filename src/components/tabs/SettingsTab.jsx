60d4116f6115f8969769e454c231428d7d95c734
varankeerthi-dev <varankeerthi@gmail.com>
2026-03-14 15:05:47 +0530

    fix: resolve build-breaking syntax error in SettingsTab.jsx

--- src/components/tabs/SettingsTab.jsx
+++ src/components/tabs/SettingsTab.jsx
@@ -1706,7 +1706,8 @@ export default function SettingsTab() {
                             </button>
                           </td>
                         </tr>
-                      ))}
+                        )
+                      })}
                     </tbody>
                   </table>
                 </div>
