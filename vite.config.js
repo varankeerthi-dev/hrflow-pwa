835e43d77fd3a85e1a0652b5624084116da0482e
varankeerthi-dev <varankeerthi@gmail.com>
2026-04-19 10:05:34 +0530

    feat: initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   base: './',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
   optimizeDeps: {
     exclude: ['@capacitor/core', '@capgo/capacitor-updater']
   },
